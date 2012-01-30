073d1ece6cac0994cd1db577f0dac4c0aa531274
XJ <zhuoxianjing@gmail.com>
2012-01-30 19:48:01 +0800

    [XJ] filter comments of N/A, #N/A and isInSameWeek validation for log date

--- tplus-chrome/log-repository.js
+++ tplus-chrome/log-repository.js
@@ -7,7 +7,7 @@ function LogRepository(data) {
 LogRepository.prototype = {
     search: function(name, endDateOfWeek) {
         var logEntries = this._extractLogEntries(name, endDateOfWeek);
-        console.log('logEntries', logEntries);
+
         return this._transformLogEntries(logEntries);
     },
     _extractLogEntries: function(name, endDateOfWeek) {
@@ -23,15 +23,16 @@ LogRepository.prototype = {
                 });
             }
         });
-        return result.slice(1);
+        return result;
     },
     _isCheckedInBy: function(description, name) {
         return description.indexOf(name) != -1
     },
-    _isInSameWeek: function(currentDate, endDateOfWeek) {
-        endDateOfWeek = new Date(endDateOfWeek);
-        endDateOfWeek = new Date(new Date(new Date(endDateOfWeek).toDateString()).getTime() + 3600*1000*24);
-        return (endDateOfWeek.getTime() - new Date(currentDate).getTime()) <= 7 * this.MILLI_SECONDS_IN_ONE_DAY;
+    _isInSameWeek: function(date, endDateOfWeek) {
+        var realEndDate = new Date(endDateOfWeek);
+        realEndDate = new Date(new Date(new Date(realEndDate).toDateString()).getTime() + 3600 * 1000 * 24);
+        var logDate = new Date(date);
+        return (realEndDate >= logDate) && ((realEndDate.getTime() - logDate.getTime()) <= 7 * this.MILLI_SECONDS_IN_ONE_DAY);
     },
     _transformLogEntries: function(logEntries) {
         var me = this;
@@ -40,28 +41,30 @@ LogRepository.prototype = {
         })), function(day) {
             return day;
         });
-        console.log('working days->', workingDays);
         return _.map(workingDays, function(day) {
             var logEntriesOfDay = _.filter(logEntries, function(logEntry) {
                 var dayOfLogEntry = me._getNumberOfDay(logEntry.date);
                 return dayOfLogEntry == day;
             });
             var commentsOfDay = _.uniq(_.map(logEntriesOfDay, function(logEntry) {
-                return me._transformComments(logEntry.description)
+                return logEntry.description.split(" ")[1];
             }));
-            return  {'dayOfWeek': day, 'comment': commentsOfDay.join(',')};
+            return  {'dayOfWeek': day, 'comment': me._transformComments(commentsOfDay)};
         });
     },
     _getNumberOfDay : function(date) {
         var dayNumberOfDate = new Date(date).getDay();
         return !dayNumberOfDate ? 7 : dayNumberOfDate;
     },
-    _transformComments: function(description) {
-        var comment = description.split(" ")[1];
-//        if (comment) {
-//            comment = comment.replace("#N/A", '');
-//        }
-        return description.split(" ")[1];
+    _transformComments: function(comments) {
+        var result = [];
+        _.each(comments, function(comment) {
+            comment = comment.replace("#N/A", '').replace('N/A', '');
+            if (comment) {
+                result.push(comment);
+            }
+        });
+        return result.join(',');
     }
 
 }
