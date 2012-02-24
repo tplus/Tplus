cf8ae0cba45acce78bce9203d103cd9cf72e5f0f
Xianjing <zhuoxianjing@gmail.com>
2012-02-24 09:35:10 +0800

    [XJ] disable public holiday

--- tplus-chrome/script/timesheet-record-builder.js
+++ tplus-chrome/script/timesheet-record-builder.js
@@ -7,7 +7,7 @@ function TimeSheetRecordsBuilder(userName, endDate,logRepositoryUrl) {
 TimeSheetRecordsBuilder.prototype = {
     build:function (callback) {
         var me = this;
-        var holidays = new HolidayRepository().findBy(this.endDate);
+        var holidays = [];//new HolidayRepository().findBy(this.endDate);
         jQuery.get(this.logRepositoryUrl, function(data) {
             var logs = new LogParser(data).filter(this.userName, this.endDate);
             callback(me.merge(logs, holidays));
